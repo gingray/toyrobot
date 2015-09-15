53ee4bb152a3ecfee9452f4886c6429929bea275
gingray <gingray.dev@gmail.com>
2015-09-15 13:29:20 +0300

    add place function and spec

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,15 +1,35 @@
 class Parser
-  TERMINALS = ['PLACE', 'MOVE', 'LEFT', 'RIGHT', 'REPORT']
+  TERMINALS = ['place', 'move', 'left', 'right', 'report']
   DIRECTIONS = ['north', 'south', 'west', 'east']
 
-  attr_accessor :tokens, :pos
+  attr_accessor :tokens, :pos, :robot, :field
 
   def initialize
     @pos = 0
   end
 
-  def input str
+  def process str
+    tokenize str
+    while @tokens.count - 1 > @pos
+      send @tokens[@pos]
+    end
+  end
+
+  def tokenize str
     str = str.downcase
     @tokens = str.split /[\s,]+/
+  end
+
+  def place
+    pos_x = next_token.to_i
+    pos_y = next_token.to_i
+    direction = next_token.to_sym
+    @robot = Robot.new({ x: pos_x, y: pos_y }, direction)
+    @field = Field.new robot
+  end
+
+  def next_token
+    @pos+= 1
+    @tokens[@pos]
   end
 end
--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,7 +1,17 @@
 describe Parser do
   let(:parser) { Parser.new }
-  let(:str) { 'PLACE 0,   0,NORTH MOVE REPORT' }
-  before { parser.input s tr}
-  it { expect(parser.tokens.count).to eq 6 }
-  it { expect(parser.pos).to eq 0 }
+  context 'tokenize' do
+    let(:str) { 'PLACE 0,   0,NORTH MOVE REPORT' }
+    before { parser.tokenize str}
+    it { expect(parser.tokens.count).to eq 6 }
+    it { expect(parser.pos).to eq 0 }
+  end
+
+  context 'place' do
+    let(:str) { 'PLACE 0, 0, NORTH' }
+    let(:robot) { parser.robot }
+    before { parser.process str}
+    it { expect(robot.position).to eq({ x:0, y: 0}) }
+    it { expect(robot.direction).to eq :north }
+  end
 end
